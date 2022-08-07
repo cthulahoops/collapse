ccf8012c7ceacbfcdb6ed786dba42a33fe23450d
Adam Kelly <adam@cthulahoops.org>
2022-08-07 12:29:11 +0100

    Kind of converted to tiles, but broken.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
-const gridSize = 30
+const gridSize = 10
+
+function main () {
+  const tiles = extractTiles()
+  displayTiles(tiles)
+  const rules = buildRules(tiles)
+
+  const canvas = document.getElementById('output')
+  canvas.width = 900
+  canvas.height = 900
+
+  const context = canvas.getContext('2d')
+
+  const wave = []
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    wave[i] = new Superposition(tiles)
+  }
+
+  //  setInterval(() => {
+  for (let i = 0; i < 100; i++) {
+    const selected = selectAndCollapse(wave)
+    if (selected < 0) {
+      return
+    }
+
+    propagate(selected, wave, rules)
+    display(context, wave, tiles)
+  }
+  // }, 1000)
+}
 
 const SAMPLE = [
   ' A  ',
-  'AAAA',
-  '   A',
-  '   A'
+  ' A  ',
+  ' A  ',
+  ' A  '
 ]
 
 function extractTiles () {
-  console.log('samel')
-  const canvas = document.getElementById('tiles')
-  canvas.width = 400
-  canvas.height = 400
-  const context = canvas.getContext('2d')
-
   const tiles = []
   for (let i = 0; i < SAMPLE.length; i++) {
     for (let j = 0; j < SAMPLE[0].length; j++) {
@@ -22,18 +45,32 @@ function extractTiles () {
         for (let x = 0; x < 3; x++) {
           const value = SAMPLE[(i + y) % SAMPLE.length][(j + x) % SAMPLE[0].length]
           pixels.push(value)
-          if (value === 'A') {
-            context.fillStyle = 'black'
-          } else {
-            context.fillStyle = '#ccccff'
-          }
-          context.fillRect(i * 40 + 10 * y, j * 40 + 10 * x, 9, 9)
         }
       }
       tiles.push(pixels.join(''))
     }
   }
-  return tiles
+  return Array.from(new Set(tiles))
+}
+
+function displayTiles (tiles) {
+  const canvas = document.getElementById('tiles')
+  canvas.width = 400
+  canvas.height = 400
+  const context = canvas.getContext('2d')
+  for (let i = 0; i < tiles.length; i++) {
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        const value = tiles[i][y * 3 + x]
+        if (value === 'A') {
+          context.fillStyle = 'black'
+        } else {
+          context.fillStyle = '#ccccff'
+        }
+        context.fillRect(i * 40 + 10 * x, 10 * y, 9, 9)
+      }
+    }
+  }
 }
 
 const Up = 'Up'
@@ -62,9 +99,31 @@ function allowedAdjacent (tile1, tile2, direction) {
   }
 }
 
+function buildRules (tiles) {
+  const rules = []
+  for (let i = 0; i < tiles.length; i++) {
+    rules.push(new Map())
+    for (const direction of [Up, Down, Left, Right]) {
+      const allowed = new Set()
+      rules[i].set(direction, allowed)
+
+      for (let j = 0; j < tiles.length; j++) {
+        if (allowedAdjacent(tiles[i], tiles[j], direction)) {
+          allowed.add(j)
+        }
+      }
+    }
+  }
+
+  return rules
+}
+
 class Superposition {
-  constructor () {
-    this.values = ['L', 'S', 'C', 'M', 'O', 'T']
+  constructor (tiles) {
+    this.values = []
+    for (let i = 0; i < tiles.length; i++) {
+      this.values.push(i)
+    }
   }
 
   entropy () {
@@ -80,85 +139,27 @@ class Superposition {
   }
 }
 
-function main () {
-  const tiles = extractTiles()
-
-  for (const tile1 of tiles) {
-    for (const tile2 of tiles) {
-      for (const direction of [Up, Down, Left, Right]) {
-        const allowed = allowedAdjacent(tile1, tile2, direction)
-        if (allowed) {
-          // console.log(tile1, tile2, direction)
-        }
-      }
-    }
-  }
-
-  const canvas = document.getElementById('output')
-  canvas.width = 900
-  canvas.height = 900
-
-  const context = canvas.getContext('2d')
-
-  const wave = []
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    wave[i] = new Superposition()
-  }
-
-  setInterval(() => {
-    const selected = selectAndCollapse(wave)
-    if (selected < 0) {
-      return
-    }
-    propagate(selected, wave)
-
-    display(context, wave)
-  }, 20)
-}
-
-function displayColor (poss) {
-  let r = 128
-  let g = 128
-  let b = 128
+function displayColor (poss, tiles) {
+  let r = 0
+  let g = 0
+  let b = 0
   for (const item of poss.values) {
-    if (item === 'L') {
-      r = r / 2
-      g = (g + 255) / 2
-      b = b / 2
-    }
-    if (item === 'S') {
-      r = r / 2
-      g = g / 2
-      b = (255 + b) / 2
-    }
-    if (item === 'C') {
-      r = (255 + r) / 2
-      g = (255 + g) / 2
-      b = b / 2
-    }
-    if (item === 'M') {
-      r = (200 + r) / 2
-      g = (200 + g) / 2
-      b = (200 + b) / 2
-    }
-    if (item === 'O') {
-      r = (0 + r) / 2
-      g = (0 + g) / 2
-      b = (128 + b) / 2
-    }
-    if (item === 'T') {
-      r = (255 + r) / 2
-      g = (255 + g) / 2
-      b = (255 + b) / 2
+    if (tiles[item][0] === 'A') {
+      r += 255
+      g += 255
+      b += 255
     }
   }
+  r /= poss.values.length
+  g /= poss.values.length
+  b /= poss.values.length
   return `rgb(${r}, ${g}, ${b})`
 }
 
-function display (context, wave) {
+function display (context, wave, tiles) {
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
-      context.fillStyle = displayColor(wave[i * gridSize + j])
+      context.fillStyle = displayColor(wave[i * gridSize + j], tiles)
       context.fillRect(i * 30, j * 30, 29, 29)
     }
   }
@@ -170,6 +171,9 @@ function selectAndCollapse (wave) {
   for (let idx = 0; idx < wave.length; idx++) {
     const possibilities = wave[idx]
     const score = possibilities.entropy()
+    if (score === 0) {
+      continue
+    }
     if (score === 1) {
       continue
     } else if (score === bestScore) {
@@ -189,57 +193,53 @@ function selectAndCollapse (wave) {
   return selected
 }
 
-function propagate (selected, wave) {
+function propagate (selected, wave, rules) {
   const changed = [selected]
 
   let count = 0
   while (changed.length > 0 && count < 30) {
     count++
     const selected = changed.pop()
-    console.log('Changed: ', selected)
+    // console.log('Changed: ', selected)
     const values = wave[selected].values
 
-    const allowed = new Set()
-    for (const item of values) {
-      if (item === 'L') {
-        allowed.add('L')
-        allowed.add('C')
-        allowed.add('M')
-      } else if (item === 'S') {
-        allowed.add('C')
-        allowed.add('S')
-        allowed.add('O')
-      } else if (item === 'C') {
-        allowed.add('C')
-        allowed.add('S')
-        allowed.add('L')
-      } else if (item === 'M') {
-        allowed.add('L')
-        allowed.add('M')
-        allowed.add('T')
-      } else if (item === 'T') {
-        allowed.add('M')
-      } else if (item === 'O') {
-        allowed.add('O')
-        allowed.add('S')
+    for (const direction of [Up, Down, Left, Right]) {
+      // Accumulate set of allowed neighbours in this direction
+      const allowed = new Set()
+      for (const tile1 of values) {
+        for (const tile2 of rules[tile1].get(direction)) {
+          allowed.add(tile2)
+        }
       }
-    }
 
-    console.log('This is ', values, ' so allowed ', allowed)
+      if (allowed.length === 0) {
+        console.log('OH NO! ABORTING')
+        return
+      }
 
-    if (allowed.length === 0) {
-      console.log('OH NO! ABORTING')
-      return
-    }
-
-    for (const idx of neighbours(selected)) {
-      const possibilities = wave[idx]
+      const neighbour = getNeighbour(selected, direction)
+      const possibilities = wave[neighbour]
       const oldEntropy = possibilities.entropy()
       possibilities.intersection(allowed)
       if (possibilities.entropy() !== oldEntropy) {
-        changed.push(idx)
+        // console.log(neighbour, ' -> ', possibilities)
+        changed.push(neighbour)
       }
     }
+  }
+}
+
+function getNeighbour (index, direction) {
+  const [i, j] = fromIndex(index)
+
+  if (direction === Up) {
+    return toIndex((gridSize + i - 1) % gridSize, j)
+  } else if (direction === Down) {
+    return toIndex((i + 1) % gridSize, j)
+  } else if (direction === Left) {
+    return toIndex(i, (gridSize + j - 1) % gridSize)
+  } else if (direction === Right) {
+    return toIndex(i, (j + 1) % gridSize)
   }
 }
 
