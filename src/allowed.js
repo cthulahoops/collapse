9acd6ddceff8e81d7ee240a80fbbf5d1492f022b
Adam Kelly <adam@cthulahoops.org>
2022-08-11 14:51:32 +0100

    Use bits to store and compute on sets in allowed

--- src/allowed.js
+++ src/allowed.js
@@ -4,12 +4,30 @@ export const Down = 'Down'
 export const Left = 'Left'
 export const Right = 'Right'
 
+class Allowed {
+  constructor () {
+    this.contents = BigInt(0)
+  }
+
+  add (value) {
+    this.contents |= BigInt(1) << BigInt(value)
+  }
+
+  has (value) {
+    return this.contents & BigInt(1) << BigInt(value)
+  }
+
+  union (other) {
+    this.contents |= other.contents
+  }
+}
+
 export function buildRules (tiles) {
   const rules = []
   for (let i = 0; i < tiles.length; i++) {
     rules.push(new Map())
     for (const direction of [Up, Down, Left, Right]) {
-      const allowed = new Set()
+      const allowed = new Allowed()
       rules[i].set(direction, allowed)
 
       for (let j = 0; j < tiles.length; j++) {
@@ -24,11 +42,9 @@ export function buildRules (tiles) {
 }
 
 export function combineAllowed (rules, tiles, direction) {
-  const allowed = new Set()
+  const allowed = new Allowed()
   for (const tile1 of tiles) {
-    for (const tile2 of rules[tile1].get(direction)) {
-      allowed.add(tile2)
-    }
+    allowed.union(rules[tile1].get(direction))
   }
   return allowed
 }
